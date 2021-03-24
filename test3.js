bec6441fb65a0e70abae1539a2a2711964da0483
hanana1253 <hanana1253@gmail.com>
2021-03-24 11:48:58 +0900

    feat: Try one more time

---
+++ test3.js
@@ -0,0 +1,40 @@
+// 3. 프로퍼티 정렬
+// 요소의 프로퍼티(id, content, completed)를 문자열 인수로 전달하면 todos의 요소를 정렬하는 함수를 작성하라.
+// 단, todos는 변경되지 않도록 하자.
+// 참고: Array.prototype.sort
+
+const todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+function sortBy(key) {
+  todos.sort((a, b) => a[key] < b[key] ? -1 : (a[key] > b[key] ? 1 : 0));
+  return todos;
+}
+
+console.log(sortBy('id'));
+/*
+[
+  { id: 1, content: 'Javascript', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 3, content: 'HTML', completed: false }
+]
+*/
+console.log(sortBy('content'));
+/*
+[
+  { id: 2, content: 'CSS', completed: true },
+  { id: 3, content: 'HTML', completed: false },
+  { id: 1, content: 'Javascript', completed: false }
+]
+*/
+console.log(sortBy('completed'));
+/*
+[
+  { id: 3, content: 'HTML', completed: false },
+  { id: 1, content: 'Javascript', completed: false },
+  { id: 2, content: 'CSS', completed: true }
+]
+*/
