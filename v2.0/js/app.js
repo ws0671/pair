0df8d33ea1d0d0afb41f845fd8295d112324051b
ws0671 <ws0671@naver.com>
2021-03-26 17:18:07 +0900

    problem update

--- v2.0/js/app.js
+++ v2.0/js/app.js
@@ -1,3 +1,19 @@
 // State
 let todos = [];
 
+todos = [
+  { id: 1, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 3, content: 'Javascript', completed: false }
+];
+
+const render = ()=>{
+  document.querySelector('.todos').innerHTML = todos.map(({id,content,completed})=>{
+    return `<li id='${id}' class="todo-item">
+        <input id="ck-${id}" class="checkbox" type="checkbox"${completed ? "checked":""}>
+        <label for="ck-${id}">${content}</label>
+        <i class="remove-todo far fa-times-circle"></i>
+      </li>`
+  }).join('');
+}
+render();
