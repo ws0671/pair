3a97caab74ec3380251f814c032cb8095b7b4371
ws0671 <ws0671@naver.com>
2021-03-29 12:55:50 +0900

    app update

--- v2.0/js/app.js
+++ v2.0/js/app.js
@@ -1,16 +1,27 @@
 // State
 let todos = [];
+const $todoInput = document.querySelector('.input-todo');
+const $todo = document.querySelector('.todos');
+
+todos = [
+  { id: 1, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 3, content: 'Javascript', completed: false }
+].sort((todo1,todo2)=>todo2.id-todo1.id);
 
 const render = ()=>{
-  document.querySelector('.todos').innerHTML = todos.map(({id,content,completed})=>{
+  $todo.innerHTML = todos.map(({id,content,completed})=>{
     return `<li id='${id}' class="todo-item">
         <input id="ck-${id}" class="checkbox" type="checkbox"${completed ? "checked":""}>
         <label for="ck-${id}">${content}</label>
         <i class="remove-todo far fa-times-circle"></i>
       </li>`
   }).join('');
+  document.querySelector('.completed-todos').textContent = todos.filter(todo => todo.completed).length + '';
+  document.querySelector('.active-todos').textContent = todos.filter(todo => !todo.completed).length + '';
 };
 
+//브라우저 켰을때 render를 가장 먼저 실행시켜라
 document.addEventListener('DOMContentLoaded', render);
 
 const generateNextId = () => {
@@ -18,19 +29,40 @@ const generateNextId = () => {
 };
 
 const addTodo = () => {
-  todos = [ { id: generateNextId(), content: document.querySelector('.input-todo').value, completed: false }, ... todos ];
+  todos = [ { id: generateNextId(), content: $todoInput.value, completed: false }, ... todos ];
   render();
 };
 
-document.querySelector('.input-todo').onkeydown = e => {
+const toggleTodo = id => {
+  todos = todos.map(todo => (todo.id === +id ? {...todo, completed: !todo.completed} : todo));
+  render();
+}
+
+$todoInput.onkeydown = e => {
   if ( e.key !== 'Enter') return;
+  if ($todoInput.value === "") return;
   addTodo();
-  document.querySelector('.input-todo').value = '';
+  $todoInput.value = '';
 };
 
-document.querySelector('.todos').onclick = e => {
+$todo.onclick = e => {
   if (!e.target.classList.contains('remove-todo')) return;
   todos = todos.filter(todo => todo.id !== +e.target.parentNode.id);
   // todos = todos.filter(todo => e.target.parentNode.id !== todo.id );
   render();
 };
+
+$todo.onchange = e => {
+  const {id} = e.target.parentNode;
+  toggleTodo(id);
+};
+
+document.getElementById('ck-complete-all').onchange = e => {
+  todos = todos.map(todo => ({ ... todo, completed: e.target.checked}));
+  render();
+};
+
+document.querySelector('.btn').onclick = () => {
+  todos = todos.filter(todo => !todo.completed);
+  render();
+}
